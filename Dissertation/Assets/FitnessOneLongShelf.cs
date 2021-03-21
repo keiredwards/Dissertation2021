6977d3570eb004c8b4035d2633ea0a55db51d2b9
Keir Edwards <keire16@hotmail.co.uk>
2021-03-21 16:23:35 +0000

    Added Support for multiple levels generated and assessed

---
+++ Dissertation/Assets/FitnessOneLongShelf.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitnessOneLongShelf : MonoBehaviour
+{
+    public int LayoutNum;
+    public GameObject ScriptHolder;
+    private int Checked = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LayoutNum = int.Parse(transform.parent.name);
+        ScriptHolder = GameObject.Find("ScriptHolder");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Checked < 2)
+        {
+
+            if (transform.parent.position.x == 0)
+            {
+
+                //Debug.Log("we at 0 baws");
+                transform.parent.position = new Vector3(0, 0, LayoutNum * 10);
+
+            }
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2f))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+                AddtoFitness();
+                //Debug.Log("WE gotta hit capn");
+            }
+            Checked++;
+            
+        }
+    }
+
+    void AddtoFitness()
+    {
+        Debug.Log(LayoutNum);
+        ScriptHolder.GetComponent<GeneticAlgorithm>().FitnessScores[LayoutNum] += 1;
+
+    }
+}
--- Dissertation/Assets/GeneticAlgorithm.cs
+++ Dissertation/Assets/GeneticAlgorithm.cs
@@ -6,74 +6,124 @@ public class GeneticAlgorithm : MonoBehaviour
 {
     public int Width;
     public int Height;
-    public int[] OneDShopFloor;
-
     public int OneLengthShelvesCount;
+    public int PopulationSize;
     public GameObject OneShelf;
     public GameObject Path;
 
+    public int[] FitnessScores;
 
-    void Start()    //Shop Floor is randomly generated.
-    {
-        int[,] ShopFloor = new int[Width, Height];
-        OneDShopFloor = new int[(Width - 1) * (Height - 1)];
-        
+
+    int[,,] GenerateNewLayout(int[,,] Layouts, int individual) {
 
         for (int x = 0; x < Width; x++)     //Create Empty 2d Array Representing Shop Floor of only Paths where 0 is a path.
         {
-            for(int y = 0; y <Height; y++)
+            for (int y = 0; y < Height; y++)
             {
-                ShopFloor[x, y] = 0;
+                Layouts[individual, x, y] = 0;
             }
         }
 
-
-        for(int x = 0; x< OneLengthShelvesCount; x++)     //Randomise Positions of Shelves and add to 2d array
+        for (int x = 0; x < OneLengthShelvesCount; x++)     //Randomise Positions of Shelves and add to 2d array
         {
             int posx = Random.Range(0, Width);
             int posy = Random.Range(0, Height);
 
-            while (ShopFloor[posx, posy] != 0)      //find a new position for shelf if not on pathv 
+            while (Layouts[individual,posx, posy] != 0)      //find a new position for shelf if not on pathv 
             {
                 posx = Random.Range(0, Width);
                 posy = Random.Range(0, Height);
             }
 
-            ShopFloor[posx, posy] = 1;
+            Layouts[individual, posx, posy] = 1;
         }
 
+        return Layouts;
+    }
+
+    int[,] ConvertToFlatArray(int[,,] Layouts, int[,] OneDLayouts, int individual)
+    {
         for (int x = 0; x < Width; ++x)     //Make 2d array into 1d for combination
         {
             for (int y = 0; y < Height; ++y)
             {
-                OneDShopFloor[x]= ShopFloor[x,y];
-                Debug.Log(x +"," + y + "=" + ShopFloor[x, y]);
+                //ebug.Log(x + "," + y);
+                //Debug.Log(x + "," + y + "=" + Layouts[individual, x, y]);
+                OneDLayouts[individual,(x+(y*Width))] = Layouts[individual,x, y];
+                
             }
         }
+
+        return OneDLayouts; 
+    }
+
+    void Start()    //Shop Floor is randomly generated.
+    {
+
+        
+
+
+
+
+        
+    
+       
+
+    }
+
+    void FindBestLayout(int[,,] Layouts, int[,] OneDLayouts)
+    {
+
+        for(int individual = 0; individual < PopulationSize; individual++)
+        {
+            FitnessScores[individual] = 0;
+            var Level = new GameObject();
+            Level.name = individual.ToString();
+            GenerateNewLayout(Layouts, individual);
+            ConvertToFlatArray(Layouts, OneDLayouts, individual);
+            DisplayLayout(OneDLayouts, Layouts, individual);
+        }
+    }
+
+    void DisplayLayout(int[,] OneDLayouts, int[,,] Layouts, int individual)
+    {
+
+        //Debug.Log(individual);
 
         for (int x = 0; x < Width; x++)     //Place Paths and Shelves Appropriately
         {
             for (int z = 0; z < Height; z++)
             {
-                if(ShopFloor[x, z] == 0)
+                if (Layouts[individual,x, z] == 0)
                 {
-                    Instantiate(Path, new Vector3(x, 0, z), Quaternion.identity);
+                    var path = Instantiate(Path, new Vector3(x, 0, z), Quaternion.identity);
+                    path.transform.parent = GameObject.Find(individual.ToString()).transform;
                 }
-                else if (ShopFloor[x, z] == 1)
+                else if (Layouts[individual,x, z] == 1)
                 {
-                    Instantiate(OneShelf, new Vector3(x, 0, z), Quaternion.identity);
+                    var Shelf = Instantiate(OneShelf, new Vector3(x, 0.5f, z), Quaternion.identity);
+                    Shelf.transform.parent = GameObject.Find(individual.ToString()).transform;
                 }
 
 
             }
         }
 
+        for (int x = 0; x < OneDLayouts.Length; x++)
+        {
+            //Debug.Log(OneDLayouts[individual,x]);
+        }
     }
-
-
 
     void Update()
     {
+        if (Time.frameCount == 1)
+        {
+            int[,,] Layouts = new int[PopulationSize, Width, Height];
+            int[,] OneDLayouts = new int[PopulationSize, Width * Height];
+            FitnessScores = new int[PopulationSize];
+            FindBestLayout(Layouts, OneDLayouts);
+        }
 
     }
 }
